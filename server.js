0c152fd2c0768e9e34e2fb1b0a830bed326ed795
T Deepthy <deepthy@flycatchtech.com>
2020-04-08 21:04:44 +0530

    Cors added

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 require('dotenv').config()
 // create express app
 const app = express();
-
+const cors = require('cors');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(cors());
 // parse application/json
 app.use(bodyParser.json())
 
