3edc670bd8307ea98a3a3c765761d8d9527a14fb
T Deepthy <deepthy@flycatchtech.com>
2020-04-19 05:57:30 +0530

    Change model employee

--- app/models/employee.js
+++ app/models/employee.js
@@ -1,20 +1,10 @@
 const mongoose = require('mongoose');
 
 const EmployeeSchema = mongoose.Schema({
-    empNo:{
-        type:String,
-        required: '{PATH} is required!',
-        unique: true
-    },
-    address: {
-        type:String,
-        required: '{PATH} is required!',
-    },
+    empNo: String,
+    address: String,
     designation: { type: mongoose.Schema.Types.ObjectId, ref: 'Designation' },
-    CTC:{
-        type:Number,
-        required: '{PATH} is required!',
-    }        
+    CTC: Number        
     
 }, {
     timestamps: true
